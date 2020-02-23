680adaafb55a926227146bef6727e051b3012967
Healdb <Healdb@users.noreply.github.com>
2020-02-23 04:12:50 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,6 @@
-<!-- Simple PHP Backdoor By DK (One-Liner Version) -->
-<!-- Usage: http://target.com/simple-backdoor.php?cmd=cat+/etc/passwd -->
-<?php $cmd = "ls"; system($cmd); echo "</pre>"; die; }?>
+<?php
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+?>
