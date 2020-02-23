844e32fd67cbf2affbe9711fc26ebf3ec1cf6f3b
Healdb <Healdb@users.noreply.github.com>
2020-02-23 07:14:20 -0500

    Update test3.php

--- test3.php
+++ test3.php
@@ -35,7 +35,7 @@ echo '</pre>';
 
 dvwaDatabaseConnect();
 
-	$query  = "SELECT * FROM `users` WHERE user='$user' AND password='$pass';";
+	$query  = "SELECT * FROM `users`;";
 	$result = @mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '.<br />Try <a href="setup.php">installing again</a>.</pre>' );
 echo '<pre>';
 print_r($result);
