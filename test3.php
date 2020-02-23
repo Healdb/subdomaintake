c9930c1df58e88fbe0f3e60b04e212a16cca7252
Healdb <Healdb@users.noreply.github.com>
2020-02-23 07:16:15 -0500

    Update test3.php

--- test3.php
+++ test3.php
@@ -38,6 +38,9 @@ dvwaDatabaseConnect();
 	$query  = "SELECT * FROM `users`;";
 	$result = @mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '.<br />Try <a href="setup.php">installing again</a>.</pre>' );
 echo '<pre>';
-print_r($result);
+while($row = mysql_fetch_array($result)) {
+    echo $row['column_name']; // Print a single column data
+    echo print_r($row);       // Print the entire row data
+}
 echo '</pre>';
 ?>
